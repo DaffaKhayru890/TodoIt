51fabeec050fa6f1c7bed5e69adca4c0194d0ab6
DaffaKhayru890 <daffakhayru@gmail.com>
2025-08-25 20:30:56 +0700

    initial commit

--- client/src/hooks/NonPrivateRoute.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Navigate } from 'react-router-dom';
-
-const NonPrivateRoute = ({children}: {children: JSX.Element}) => {
-  const token = localStorage.getItem("User");
-  return !token ? children : <Navigate to="/dashboard/inbox" replace />
-}
-
-export default NonPrivateRoute
--- client/src/hooks/PrivateRoute.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Navigate } from 'react-router-dom';
-
-const PrivateRoute = ({children}: {children: JSX.Element}) => {
-  const token = localStorage.getItem("User");
-  return token ? children : <Navigate to="/login" replace />
-}
-
-export default PrivateRoute
